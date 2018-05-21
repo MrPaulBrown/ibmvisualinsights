28ceba086e21be1b8485fad36912e7e8aa68b087
paulesbrown <paul@siddlebrown.co.uk>
2018-05-21 16:21:19 +0100

    Initial version

---
+++ VISimulator/README.md
@@ -0,0 +1,62 @@
+# VISimulator script
+
+This script simulates sending a sequence of images to Visual Insights for scoring.
+
+## Set-up
+
+Set up the environment and common libraries as per the general README.md
+
+This script also requires the Kivy UI framework.  Follow these [installation instructions](https://kivy.org/docs/installation/installation.html) to install Kivy
+
+### Models deployed in IBM Visual Insights
+
+These scripts require at least one model to be deployed in IBM Visual Insights and accessible via the uploadScoreImage API
+
+### Config file
+
+Modify the config file (VIWatcherCloud.config) to set the following options:
+
+[Images]  
+Directory:  **Set to the local path location of your images**  
+WildcardPattern: **Set to match to a wildcard pattern**  
+
+[Cloud]  
+URL: **Set to Visual Insights uploadScoreImage URL**  
+Tenant: **Set to desired tenant ID**  
+Cell: **Set to scoring cell for classification model**  
+Product: **Set to product type code for classification model**  
+ModelType: **Set to 'cls' for a classification model and 'od' for an object detection model**  
+
+## Usage
+
+Run from python, e.g.
+
+`python VISimulator.py`
+
+The script will launch a UI to allow the user to configure the simulation.  In addition, the setting of the "Directory" path in the configuration will govern the simulator behavior:
+
++ If the Directory only contains images (no sub-directories) then the simulator will pick images from random from the Directory.  
++ If the directory contains sub-directories of images then the UI will be configured to allow the user to 'balance' the images from each sub-directory.  At each iteration, the simulator will pick a sub-directory based on the relative balance of all sub-directories and then choose an image at random from that folder.  
+
+The script will create a window showing the current scored image.  This will update as each new image is scored.
+
+UI Controls
+
+The user can control:
+
++ the request rate (in requests per minute)  
++ the duration of the simulation (in minutes)  
++ starting and stopping of the simulation  
+
+Commands:  
+Esc: Quit  
+
+### Explanation of output
+
+Score time is shown as the number of s in the bottom corner of the capture window
+
+Classification results are displayed in the scoring window as the top class and confidence scores
+
+Object detection results are displayed as a set of boxes on the image.  The width of the box represents the confidence level.  The color shows the object detected (reflected by the displayed key)
+
+The UI also provides feedback on the image scored, the response status and response time.  
---
+++ VISimulator/VISimulatorApp.py
@@ -0,0 +1,458 @@
+import time
+import sys
+import threading
+from kivy.clock import Clock, mainthread
+from configparser import SafeConfigParser
+import re
+import os
+import glob
+import requests
+import json
+import random
+import cv2
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.garden.bar import Bar
+from kivy.uix.label import Label
+from kivy.uix.slider import Slider
+
+default_config = 'VISimulator.config'
+no_defect_path = ''
+defect_path = ''
+image_wildcard = ''
+
+# Palette used by detection annotation
+color_palette = [
+    (255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,0,255), (0,255,255),
+    (128,0,0), (0,128,0), (0,0,128), (128,128,0), (128,0,128), (0,128,128),
+    (64,0,0), (0,64,0), (0,0,64), (64,64,0), (64,0,64), (0,64,64),
+    (192,0,0), (0,192,0), (0,0,192), (192,192,0), (192,0,192), (0,192,192)
+    ]
+
+# Parse edge scoring response object (classification)
+def parseResponse(text):
+
+    # Set best detection and confidence
+    best_det = None
+    best_conf = 0.0
+
+    try:
+        # Parse the response
+        parsed = json.loads(text)
+
+        # Iterate through the detections, finding the one with the highest confidence
+        dets = parsed['detections']
+        if dets != None:
+            if len(dets) == 1:
+                prob_types = dets[0]['probableTypes']
+                for det in prob_types:
+                    det_type = det['type']
+                    det_conf = float(det['confidence'])
+                    if det_conf > best_conf:
+                        best_det = det_type
+                        best_conf = det_conf
+
+    except ValueError:  # includes simplejson.decoder.JSONDecodeError
+        # If text can't be parsed then print text to console
+        print("Response Error: {}", text)
+
+    return (best_det, best_conf)
+
+# Parse the response from the OD model
+def parseODResponse(text):
+
+    dets = None
+    try:
+        # Parse and return the detections object
+        parsed = json.loads(text)
+        dets = parsed['detections']
+
+    except ValueError:  # includes simplejson.decoder.JSONDecodeError
+        # If text can't be parsed then print text to console
+        print("Response Error: {}", text)
+
+    return dets
+
+# Draws the text in the window
+def showText(img, text, x, y, color):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, text, (x, y), font, 0.6, color, 2, cv2.LINE_AA)
+
+def showDuration(frame, duration):
+    if duration > 0.0:
+        showText(frame, "{:.2f} s".format(duration), 560, 465, (0, 255, 0))
+
+# Draws the classification result
+def showClass(frame, det, conf):
+    showText(frame, "{} ({:2.1f}%)".format(det, conf), 5, 25, (0, 255, 0))
+
+# Draws a detection rectangle
+def drawRect(img, xmin, ymin, xmax, ymax, color, thickness):
+    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, thickness)
+
+# Draws a line for the key
+def drawKeyLine(img, y, width, color):
+    cv2.line(img, (10, y-10), (10+width, y-10), color, 2)
+
+# globals
+max_color_num = 0
+colors = {}
+max_pos = 80
+positions = {}
+
+def getColor(det):
+    global max_color_num, colors
+    if det in colors:
+        return colors[det]
+    else:
+        color = color_palette[max_color_num]
+        colors[det] = color
+        max_color_num += 1
+        return color
+
+def getPosition(det):
+    global max_pos, positions
+    if det in positions:
+        return positions[det]
+    else:
+        pos = max_pos
+        positions[det] = pos
+        max_pos += 20
+        return pos
+
+
+# Shows the detections
+def showDets(frame, cropped_frame, dets):
+    global colors
+
+    for d in dets:
+        # Get det and confidence
+        probableTypes = d['probableTypes']
+        det = probableTypes[0]['type']
+        conf = probableTypes[0]['confidence']
+
+        if det != '':
+            # get positions
+            position = d['position']
+            x = position['x']
+            y = position['y']
+            w = position['width']
+            h = position['height']
+
+            color = getColor(det)
+
+            # Draw the detection rectangle
+            drawRect(cropped_frame, x, y, x+w, y+h, color, int((conf / 100) * 4) + 1)
+
+    # Draw the keys
+    for det, color in colors.items():
+        y = getPosition(det)
+        drawKeyLine(frame, y, 10, color)
+        showText(frame, "{}".format(det), 25, y, color)
+
+def cloud_score_image(image_path, config, score_type):
+
+    # Pick up options
+    url = config['Cloud']['URL']
+    cell = config['Cloud']['Cell']
+    product = config['Cloud']['Product']
+
+    # Open file and load into content
+    data = open(image_path, 'rb').read()
+
+    # Request parameters
+    params = {'productType': product, 'cell': cell,
+        'user': os.environ['VIUSER'], 'tenant': config['Cloud']['Tenant'],
+        'solution': 'vi'}
+
+    # Request headers
+    headers = {'user': os.environ['VIUSER'], 'APIKey':os.environ['VIAPIKEY'], 'Content-Type': 'application/binary' }
+
+    # score against the Cloud Scoring API
+    try:
+        r = requests.post(url, params=params, headers=headers, data=data)
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+    return r
+
+def displayImage(image_path):
+
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    cv2.imshow("score", img)
+
+    return img
+
+def updateImage(img, det, conf, dets, duration):
+
+    # Show the class or draw the detections on the snapshot
+    if det:
+        showClass(img, det, conf)
+    if dets:
+        showDets(img, img, dets)
+
+    showDuration(img, duration)
+
+    cv2.imshow("score", img)
+
+class Timer:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+
+class VISimulator(BoxLayout):
+
+    stop = threading.Event()
+
+    def __init__(self, **kwargs):
+        super(VISimulator, self).__init__(**kwargs)
+
+        # Create list of sliders
+        rates_box = self.ids.rates_box
+
+        img_path = config['Images']['Directory']
+
+        self.sliders = {}
+
+        for root, subdirs, files in os.walk(img_path):
+            for subdir in subdirs:
+                # Create layout
+                slider_layout = BoxLayout()
+                slider_layout.orientation = 'horizontal'
+                slider_layout.size_hint_y = 0.15
+                slider_layout.height = '48dp'
+
+                # Create label
+                slider_label = Label(text=subdir)
+
+                # Create slider
+                slider = Slider()
+                slider.min = 0.0
+                slider.max = 1.0
+                slider.value = 0.5
+                slider.step = 0.1
+
+                # Create slider value
+                slider_val = Label(text='{:.1f}%'.format(slider.value))
+
+                # Add widgets
+                slider_layout.add_widget(slider_label)
+                slider_layout.add_widget(slider)
+                slider_layout.add_widget(slider_val)
+
+                self.ids.rates_box.add_widget(slider_layout)
+
+                # Add to sliders dictionary
+                self.sliders[subdir] = slider
+
+    def updateStatus(self, status):
+        self.ids.response_status.text = str(status)
+        if str(status) == "200":
+            self.ids.response_status.color = (0,1,0,1)
+        else:
+            self.ids.response_status.color = (1,0,0,1)
+
+    def updateResponseTime(self, time):
+        self.ids.response_time.text = "{:10.4f}".format(time)
+        if time > 5:
+            self.ids.response_time.color = (1,0,0,1)
+        elif time > 2:
+            self.ids.response_time.color = (1,1,0,1)
+        else:
+            self.ids.response_time.color = (0,1,0,1)
+
+    def updateImage(self, src):
+        self.ids.image_path.text = src
+
+        # img = Image()
+        # img.source='blank.png'
+        # self.ids.image_layout.add_widget(img)
+
+    @mainthread
+    def updateResults(self, status, responseTime, imagePath, responseText):
+        self.updateStatus(status)
+        self.updateResponseTime(responseTime)
+
+    @mainthread
+    def updateProgress(self, img_count, pct):
+        if img_count == -1:
+            self.ids.progress.text = 'Not Running'
+        else:
+            self.ids.progress.text = 'Progress: {} images, {:.1f}%'.format(img_count, pct)
+
+        self.ids.pb.value = pct
+
+    def prepareImages(self):
+
+        image_wildcard = config['Images']['WildcardPattern']
+
+        self.file_dict = {}
+        if not self.sliders:
+            self.files = glob.glob(os.path.join(config['Images']['Directory'], image_wildcard))
+        else:
+            for key, value in self.sliders.items():
+                self.file_dict[key] = glob.glob(os.path.join(config['Images']['Directory'], key, image_wildcard))
+
+    def getImage(self):
+
+        img_set = None
+        # No sliders - return image from the path
+        if not self.sliders:
+            img_set = self.files
+        else:
+            # Sliders - get cumulative sum
+            keys = list(self.sliders.keys())
+            sum = 0.0
+            for key, value in self.sliders.items():
+                sum += value.value
+
+            group = None
+            if sum == 0.0:
+                # All sliders at zero - select file at random
+                img_set = self.file_dict[random.choice(keys)]
+            else:
+                # Find the key to use
+                rand = random.random() * sum
+                cum = 0.0
+                for key, value in self.sliders.items():
+                    cum += value.value
+                    if rand < cum:
+                        img_set = self.file_dict[key]
+                        break
+
+        return random.choice(img_set)
+
+
+    def simulation(self):
+
+        # print ("Starting simulation")
+
+        start_time = time.time()
+        last_time = start_time
+        current_duration = 0
+        # print("Start Time: %s", time.ctime(start_time))
+
+        self.prepareImages()
+
+        num_images = 0
+
+        # Repeat until duration finishes
+        while not(self.stop.is_set()) and \
+            ((time.time() - start_time) < (self.ids.durationSlider.value * 60)):
+
+            required_duration = 60 / self.ids.requestRateSlider.value
+
+            # Select image at random based on defect rate
+            img_path = self.getImage()
+
+            score_type = config['Cloud']['ModelType']
+
+            score_time = time.clock()
+            r = cloud_score_image(img_path, config, score_type)
+            duration = time.clock() - score_time
+
+            if r.status_code == 200:
+
+                dets = None
+                det = None
+                conf = None
+
+                if score_type == 'cls':
+                    # Get det and conf from classifier
+                    (det, conf) = parseResponse(r.text)
+                else:
+                    # Get dets from OD response
+                    dets = parseODResponse(r.text)
+
+                # Display the scored image
+                img = displayImage(img_path)
+                updateImage(img, det, conf, dets, duration)
+
+            else:
+                # Score failed
+                print("{}: {}".format(r.status_code, r.text))
+
+            current_time = time.time()
+            current_duration = current_time - last_time
+
+            progress = ((current_time - start_time) / (self.ids.durationSlider.value * 60)) * 100
+
+            self.updateProgress(num_images + 1, progress)
+
+            sleep_int = 0.1
+            if current_duration < required_duration:
+                sleep_int = required_duration - current_duration
+
+            time.sleep(sleep_int)
+
+            last_time = time.time()
+
+            # Get keyboard input
+            k = cv2.waitKey(1)
+            action = False
+
+            if k%256 == 27:
+                # ESC pressed - break from loop
+                print("Escape hit, closing...")
+                observer.stop()
+                break
+
+            num_images += 1
+
+        # print ("Exiting simulation")
+        self.stopSimulation()
+
+    def startSimulation(self):
+        self.stop.clear()
+        self.ids.btn.background_color = (1, 0, 0, 0.5)
+        self.ids.btn.text = 'Stop Simulation'
+        threading.Thread(target=self.simulation).start()
+
+    def stopSimulation(self):
+        self.stop.set()
+        self.ids.btn.background_color = (0, 1, 0, 0.5)
+        self.ids.btn.text = 'Start Simulation'
+        if self.ids.btn.state == 'down':
+            self.ids.btn.state = 'normal'
+        self.updateProgress(-1, 0.0)
+
+    def click(self):
+        if self.ids.btn.state == 'down':
+            self.startSimulation()
+        else:
+            self.stopSimulation()
+
+
+
+class VISimulatorApp(App):
+
+    scorer = None
+
+    def build(self):
+        return VISimulator()
+
+    def on_stop(self):
+        # The Kivy event loop is about to stop, set a stop signal;
+        # otherwise the app window will close, but the Python process will
+        # keep running until all secondary threads exit.
+        self.root.stop.set()
+
+    @mainthread
+    def updateResults(self, status, responseTime, imagePath, responseText):
+        return self.root.updateResults(status, responseTime,imagePath, responseText)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    # Parse config file
+    config = SafeConfigParser()
+    config_path = args[0] if args else default_config
+    config.read(config_path)
+
+    app = VISimulatorApp()
+
+    app.run()
