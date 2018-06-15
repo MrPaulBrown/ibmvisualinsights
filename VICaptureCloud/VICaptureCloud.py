Fixed scaling issues

VICaptureCloud/VICaptureCloud.py
@@ -130,11 +130,11 @@ def showClass(frame, det, conf):
 def drawRect(img, xmin, ymin, xmax, ymax, color, conf):
     thickness = int((conf / 100) * 4) + 1
     height = np.size(img, 0)
-    thick = thickness * int(height / SCALE_SIZE)
+    thick = thickness * max(1, int(height / SCALE_SIZE))
     cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, thick)
     if OD_LABELS:
-        scale = max(1, int(height / SCALE_SIZE) - 1)
-        thickness = int(height / SCALE_SIZE) * 2
+        scale = max(1, int(height / SCALE_SIZE))
+        thickness = min(max(1, int(height / SCALE_SIZE) -1), 8)
         drawText(img, "{:2d}%".format(int(conf)), xmax + 5, ymax, scale, color, thickness)
 
 # Draws a line for the key
@@ -144,7 +144,7 @@ def drawKeyLine(img, ypct, widthpct, color):
     y = int(height * (ypct - 0.01))
     xmin = int(width * 0.01)
     xmax = int(width * (widthpct - 0.01))
-    thick = 4 * int(height / SCALE_SIZE)
+    thick = 4 * max(1, int(height / SCALE_SIZE))
     cv2.line(img, (xmin, y), (xmax, y), color, thick)
 
 # globals

VICaptureEdge/VICaptureEdge.py
@@ -153,7 +153,7 @@ def showClass(frame, det, conf):
 def drawRect(img, xmin, ymin, xmax, ymax, color, conf):
     thickness = int((conf / 100) * 4) + 1
     height = np.size(img, 0)
-    thick = thickness * int(height / SCALE_SIZE)
+    thick = thickness * max(1, int(height / SCALE_SIZE))
     cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color, thick)
     if OD_LABELS:
         scale = max(1, int(height / SCALE_SIZE) - 1)
@@ -167,7 +167,7 @@ def drawKeyLine(img, ypct, widthpct, color):
     y = int(height * (ypct - 0.01))
     xmin = int(width * 0.01)
     xmax = int(width * (widthpct - 0.01))
-    thick = 4 * int(height / SCALE_SIZE)
+    thick = 4 * max(1, int(height / SCALE_SIZE))
     cv2.line(img, (xmin, y), (xmax, y), color, thick)
 
 # globals